02f0b2eba291e49c7bd303badbc7a4e25f7a64d2
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-09-23 22:55:17 +0530

    development

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -33,7 +33,7 @@ export const Home: React.FC = () => {
             </div>
             <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-32">
                 <div className="lg:text-center flex flex-col items-center">
-                    <h2 className="text-red-500 font-semibold tracking-wide uppercase text-4xl">Currently in developemtn</h2>                    
+                    <h2 className="text-red-500 font-semibold tracking-wide uppercase text-4xl">Currently in development</h2>                    
                     <h2 className="text-custom-primary font-semibold tracking-wide uppercase text-4xl">Welcome To</h2>
                     <p className="mt-2 leading-8 font-extrabold tracking-tight text-custom-dark sm:text-4xl"> 
                         <img
