4f415633cada803fbf88be3fa6da3e8e254591e7
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-10-04 20:59:34 +0530

    done

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,9 +1,5 @@
 import { Navbar } from '../components/Navbar';
 import { Home } from '../components/Home';
-<<<<<<< HEAD
-=======
-import { Games } from '../components/Games';
->>>>>>> ed2a81fbf23884c6c66cb0313047d3f44d65e677
 import { Contribute } from '../components/Contribute';
 
 const Landing = () => {
@@ -13,12 +9,6 @@ const Landing = () => {
         <section id="Home" className='w-screen m-0 p-0'>
             <Home/>
         </section>
-<<<<<<< HEAD
-=======
-        <section id="Games" className='w-screen m-0 p-0'>
-            <Games/>
-        </section>
->>>>>>> ed2a81fbf23884c6c66cb0313047d3f44d65e677
         <section id="Contribute" className='w-screen m-0 p-0'>
             <Contribute/>
         </section>
