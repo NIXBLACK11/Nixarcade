73135a01ac7bbda78d780b4886c294f5c2340d75
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-10-07 19:38:13 +0530

    updates

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -5,8 +5,10 @@ import { WalletData } from "okto-sdk-react";
 import TiltWrapper from "../components/TiltWrapper";
 import { generateToken } from "../utils/generateToken";
 import { saveJWT } from "../utils/jwt-storage";
+import { useNavigate } from "react-router-dom";
 
 export const Games = () => {
+    const navigate = useNavigate();
     const [wallets, setWallets] = useState<WalletData>();
     // const okto = useOkto();
     const saveToken = async () => {
@@ -63,6 +65,7 @@ export const Games = () => {
                                 const success = await makeTransaction();
                                 if(success) {
                                     saveToken();
+                                    navigate("https://ludofam.nixarcade.fun");
                                 }
                             }}
                         >
